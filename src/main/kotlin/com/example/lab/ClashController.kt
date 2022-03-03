98cc61b111627e0ebbd15822eb9ffd95206a67ac
Trokay Oleg <59147112+CoralCargo0@users.noreply.github.com>
2022-03-03 21:41:51 +0300

    Update code

--- src/main/kotlin/com/example/lab/ClashController.kt
+++ src/main/kotlin/com/example/lab/ClashController.kt
@@ -1,14 +1,15 @@
 package com.example.lab
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.*
 
-@RestController
+@Controller
 class ClashController {
 
-    @GetMapping(value = ["/calculate"])
+    @GetMapping("/calculate")
     fun calculate(
+        model: Model,
         @RequestParam(value = "weightOfFirst", defaultValue = "0.0") weightOfFirst: String,
         @RequestParam(value = "speedOfFirst", defaultValue = "0.0") speedOfFirst: String,
         @RequestParam(value = "weightOfSecond", defaultValue = "0.0") weightOfSecond: String,
@@ -23,66 +24,22 @@ class ClashController {
                 speedOfSecond.toDouble()
             )
         } catch (e: Throwable) {
-            return init()
+            return init(model)
         }
+        model.apply {
+            addAttribute("weightOfFirst", weightOfFirst)
+            addAttribute("speedOfFirst", speedOfFirst)
+            addAttribute("weightOfSecond", weightOfSecond)
+            addAttribute("speedOfSecond", speedOfSecond)
+            addAttribute("result", clash.calculateSpeedAfterClash())
 
-        return "<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<form action=\"http://localhost:8080/calculate\">\n" +
-                "  <label for=\"weightOfFirst\">Масса 1</label>\n" +
-                "  <br>\n" +
-                "  <input type=\"text\" id=\"weightOfFirst\" name=\"weightOfFirst\" value = \"$weightOfFirst\">\n" +
-                "  <br>\n" +
-                "  <label for=\"speedOfFirst\">Скорость 1</label>\n" +
-                "  <br>\n" +
-                "  <input type=\"text\" id=\"speedOfFirst\" name=\"speedOfFirst\" value = \"$speedOfFirst\">\n" +
-                "  <br>\n" +
-                "  <label for=\"weightOfSecond\">Масса 2</label>\n" +
-                "  <br>\n" +
-                "  <input type=\"text\" id=\"weightOfSecond\" name=\"weightOfSecond\" value = \"$weightOfSecond\">\n" +
-                "  <br>\n" +
-                "  <label for=\"speedOfSecond\">Скорость 2</label>\n" +
-                "  <br>\n" +
-                "  <input type=\"text\" id=\"speedOfSecond\" name=\"speedOfSecond\" value = \"$speedOfSecond\">\n" +
-                "  <br>\n" +
-                "  <br>\n" +
-                "  <label for=\"speedAfterClash\">Скорость после столкновения = ${clash.calculateSpeedAfterClash()}</label>\n" +
-                "  <br>\n" +
-                "  <br>\n" +
-                "  <input type=\"submit\" value=\"Посчитать\"> </form>\n" +
-                "</body>\n" +
-                "\n" +
-                "</html>"
+        }
+        return "clashOfTwo"
     }
 
     @GetMapping(value = ["/"])
-    fun init(): String {
-        return "<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<form action=\"http://localhost:8080/calculate\">\n" +
-                "  <label for=\"weightOfFirst\">Масса 1</label>\n" +
-                "  <br>\n" +
-                "  <input type=\"text\" id=\"weightOfFirst\" name=\"weightOfFirst\">\n" +
-                "  <br>\n" +
-                "  <label for=\"speedOfFirst\">Скорость 1</label>\n" +
-                "  <br>\n" +
-                "  <input type=\"text\" id=\"speedOfFirst\" name=\"speedOfFirst\">\n" +
-                "  <br>\n" +
-                "  <label for=\"weightOfSecond\">Масса 2</label>\n" +
-                "  <br>\n" +
-                "  <input type=\"text\" id=\"weightOfSecond\" name=\"weightOfSecond\">\n" +
-                "  <br>\n" +
-                "  <label for=\"speedOfSecond\">Скорость 2</label>\n" +
-                "  <br>\n" +
-                "  <input type=\"text\" id=\"speedOfSecond\" name=\"speedOfSecond\">\n" +
-                "  <br>\n" +
-                "  <br>\n" +
-                "  <label for=\"speedAfterClash\">Скорость после столкновения</label>\n" +
-                "  <br>\n" +
-                "  <br>\n" +
-                "  <input type=\"submit\" value=\"Посчитать\"> </form>\n" +
-                "</body>\n" +
-                "\n" +
-                "</html>"
+    fun init(model: Model): String {
+        model.addAttribute("result", "")
+        return "clashOfTwo"
     }
 }
